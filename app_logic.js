456350c4efacd4b4666db37f430744b0ce69dc74
bengkelgadget-code <bengkelgadget.l2@gmail.com>
2026-05-07 19:12:33 +0800

    Update app_logic.js

--- app_logic.js
+++ app_logic.js
@@ -1,4 +1,3 @@
-<script>
     document.addEventListener('DOMContentLoaded', function() {
         activeRole = 'Admin'; 
         document.getElementById('loginView').classList.add('hidden');
@@ -744,4 +743,3 @@
     window.openAddKategoriGameModal = openAddKategoriGameModal; window.closeAddKategoriGameModal = closeAddKategoriGameModal; window.submitAddKategoriGame = submitAddKategoriGame;
     window.openSmartPasteModal = openSmartPasteModal; window.closeSmartPasteModal = closeSmartPasteModal; window.submitSmartPaste = submitSmartPaste;
     window.populateUmumSettings = populateUmumSettings; window.previewUmumLogo = previewUmumLogo; window.submitPengaturanUmum = submitPengaturanUmum;
-</script>
